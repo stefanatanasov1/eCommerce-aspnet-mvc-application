08fca30a2f19ddabc38ea7789ae290e6f4c62ab1
satanasov <s.atanasov@blubito.com>
2023-04-20 22:27:50 +0300

    progress

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProfilePictureUrl { get; set; }
+        public int FullName { get; set; }
+        public int Biography { get; set; }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+
+        public int Id { get; set; }
+        public int ProfilePictureUrl { get; set; }
+        public int FullName { get; set; }
+        public int Biography { get; set; }
+    }
+}
